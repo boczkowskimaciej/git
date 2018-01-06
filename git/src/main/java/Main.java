ecf07d1fce2796ea94ba9059b15045972b68ef5d
Maciek Boczkowski <boczkowski.maciej@gmail.com>
2018-01-07 00:12:28 +0100

    2 zmiana napisu w git11

--- git/src/main/java/Main.java
+++ git/src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Zmiana w main git12");
+        System.out.println("Jednak zmienie na git11");
+        System.out.println("Wersja ostateczna");
 
     }
 }
