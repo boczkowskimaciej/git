df4feba9b280a28928ad9cdce46b7bcd9a14bdba
Maciek Boczkowski <boczkowski.maciej@gmail.com>
2018-01-07 13:05:03 +0100

    5 Try push local branch Class2 to remote server

---
+++ git/src/main/java/Class2.java
@@ -0,0 +1,6 @@
+public class Class2 {
+    public static void main(String[] args) {
+        System.out.println("Class2 from git11");
+        System.out.println("Try push local branch Class2 to remote server");
+    }
+}
